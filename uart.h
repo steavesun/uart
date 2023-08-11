041964906b303a351ed6ebdede228a50cf09b4ec
OH SUN CHUL <ispo3695@naver.com>
2023-08-12 01:33:29 +0900

    header file on uart.cpp

--- uart.h
+++ uart.h
@@ -1,3 +1,5 @@
+#define MAX 1024
+
 #include <stdio.h>
 #include <unistd.h>
 #include <string>
@@ -8,10 +10,13 @@
 #include <string.h>
 #include <pthread.h>
 
+
+
 // Library for Uart
 #include <asm/termbits.h>
 
-using namespace std;
+using namespace std; 
+
 
 class UART
 {
@@ -19,32 +24,36 @@ class UART
     // Field
 
 private:
+  
     struct termios2 UartTerm;
-    string Target;
-
+    char File[1024]; 
     int BaudRate;
-    int fd;
+    int Uartfd;
+    char RxBuffer[1024] = {0,};
 
     // Method
 public:
+  
     UART(int, int);
     ~UART();
+    UART* Get_This(); 
+    
     void Set_BaudRate(int);
     int Get_BaudRate();
+     void Transmit(char *) const;
+     char Receive(); 
 
-    void Transmit(char *) const;
-    void Receive(char *);
 };
+
+
+
 
 UART ::UART(int Number, int BaudRate)
 {
 
-    Target = "/dev/ttyS" + to_string(Number);
-
-    fd = open(Target.c_str(), O_RDWR | O_NOCTTY);
-
-    // it means what ?
-    ioctl(fd, TCGETS2, &UartTerm);
+    sprintf(File,"/dev/ttyS%d",Number); 
+    Uartfd = open(File, O_RDWR | O_NOCTTY);
+    ioctl(Uartfd, TCGETS2, &UartTerm);
     UartTerm.c_cflag &= ~CBAUD;
     UartTerm.c_cflag |= BOTHER;
     UartTerm.c_cflag |= CS8;
@@ -52,33 +61,39 @@ UART ::UART(int Number, int BaudRate)
     UartTerm.c_cflag |= CREAD;
     UartTerm.c_ospeed = BaudRate;
     UartTerm.c_ispeed = BaudRate;
-    ioctl(fd, TCSETS2, &UartTerm);
+    ioctl(Uartfd, TCSETS2, &UartTerm);
 }
 
-void UART ::Transmit(char *Buffer) const
+void UART ::Transmit(char* Buffer) const
 {
 
-    int state = write(fd, Buffer, strlen(Buffer));
+    int state = write(Uartfd, Buffer, strlen(Buffer));
 
     if (state == -1)
     {
 
         printf("Transmit Error Code = %d\n", state);
     }
-}
-void UART ::Receive(char *Buffer)
-{
 
-    int state = read(fd, Buffer, sizeof(Buffer));
-    if (state == -1)
-    {
+}
+char UART :: Receive()
+{
+   char Buffer;
+   int state = read(Uartfd, &Buffer, 1);
+   if (state == -1)
+   {
         printf("Receive Error Code = %d\n", state);
-    }
+   }
+
+   return Buffer; 
+
+
 }
 
 void UART ::Set_BaudRate(int BaudRate)
 {
-
+    
+    
     UartTerm.c_ospeed = BaudRate;
     UartTerm.c_ispeed = BaudRate;
     this->BaudRate = BaudRate;
@@ -89,8 +104,22 @@ int UART ::Get_BaudRate()
     return BaudRate;
 }
 
-UART::~UART()
+UART* UART :: Get_This()
 {
 
-    close(fd);
+return this; 
+
 }
+
+UART::~UART()
+{
+    
+    cout << "--- Destructing Object : " << this << " ---" << endl;  
+     close(Uartfd);
+    
+  
+}
+
+
+
+
